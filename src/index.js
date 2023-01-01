f0c3607d8d8311f908ae816d1c4311b6d2353110
Lucky Patel <113097395+lucky7xx@users.noreply.github.com>
2023-01-01 12:15:39 +0530

    name api updated

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import { Route, BrowserRouter } from "react-router-dom";
 import "./index.css";
 // import MyComponent from "./Rating/Rating";
-// import App from "./ApiFetch/App";
+import App from "./ApiFetch/App";
 // import BatchUsage from "./ApiFetch/BatchUsage";
-import WeatherCard from "./WeatherApp/WeatherCard";
+// import WeatherCard from "./WeatherApp/WeatherCard";
 // import WeatherApp from "./WeatherApp/WeatherApp";
 // import Rating from "./Rating/Rating";
 // import App from "./Router/App";
@@ -15,6 +15,6 @@ import WeatherCard from "./WeatherApp/WeatherCard";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <WeatherCard />
+    <App />
   </BrowserRouter>
 );
